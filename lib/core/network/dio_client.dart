4600b6d26bf8e7e8043b2576274c5b0d92f6da02
alif.algibran <alifalgibran147@gmail.com>
2023-11-04 21:32:10 +0700

    remove credential

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:klontong/core/utlis/constants.dart';
 
 // we create dio client as Lazy singleton, so whenever this class
 // is called, it just calls the latest instance if it already instanciated
 // from other class, if there is no instanciate before, it should instanciate
 // for the first time.
 class DioClient with DioMixin {
-  static const _url =
-      'https://crudcrud.com/api/33a2b5432aa14917ba393d73879f648b/';
+  static const _url = 'https://crudcrud.com/api/${Constants.crudCredential}/';
 
   // instanciate
   static DioClient init() => DioClient._();
